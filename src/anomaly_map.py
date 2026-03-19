30e16b7ea114d059399f6b92eaa69d72568e16d2
barkin <63337752+barkinadiguzel@users.noreply.github.com>
2026-03-19 18:11:01 +0300

    Create anomaly_map.py

---
+++ src/anomaly_map.py
@@ -0,0 +1,15 @@
+import torch
+import torch.nn.functional as F
+
+def image_level_score(Mo, local_pool_size=7):
+    if Mo.dim() == 2:
+        Mo = Mo.unsqueeze(0).unsqueeze(0)  
+    elif Mo.dim() == 3:
+        Mo = Mo.unsqueeze(0)  
+
+    Mo_local = F.max_pool2d(Mo, kernel_size=local_pool_size, stride=1, padding=local_pool_size//2)
+
+    eta_max = Mo_local.max()
+    eta_mean = Mo_local.mean()
+
+    return eta_max, eta_mean
