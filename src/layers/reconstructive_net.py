6d4cc1b41a2511fc027ddd92e6f87bb279cbaccf
barkin <63337752+barkinadiguzel@users.noreply.github.com>
2026-03-19 18:03:53 +0300

    Create reconstructive_net.py

---
+++ src/layers/reconstructive_net.py
@@ -0,0 +1,25 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+from layers.unet_blocks import ConvBlock, UpBlock
+
+class ReconstructiveNet(nn.Module):
+    def __init__(self, in_channels=3, base_channels=64):
+        super().__init__()
+        self.enc1 = ConvBlock(in_channels, base_channels)
+        self.enc2 = ConvBlock(base_channels, base_channels*2)
+        self.enc3 = ConvBlock(base_channels*2, base_channels*4)
+        
+        self.dec3 = UpBlock(base_channels*4, base_channels*2)
+        self.dec2 = UpBlock(base_channels*2, base_channels)
+        self.dec1 = nn.Conv2d(base_channels, in_channels, kernel_size=1)
+        
+    def forward(self, x):
+        e1 = self.enc1(x)
+        e2 = self.enc2(F.max_pool2d(e1,2))
+        e3 = self.enc3(F.max_pool2d(e2,2))
+        
+        d3 = self.dec3(e3, e2)
+        d2 = self.dec2(d3, e1)
+        out = self.dec1(d2)
+        return out  
